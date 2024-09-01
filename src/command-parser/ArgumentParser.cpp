7d7964097b78b424a7b422230fe14ee2a4fbcf49
saku shirakura <saku@sakushira.com>
2024-09-01 18:41:08 +0900

    [add] argument parserを追加。

---
+++ src/command-parser/ArgumentParser.cpp
@@ -0,0 +1,90 @@
+#include "ArgumentParser.h"
+#include <regex>
+
+void com_sakushira::cpp_lib::ArgumentParser::parse(const std::vector<std::string> &args_) {
+    // 引数の種別が名前付き引数かどうかのフラグ。
+    bool named_arg_flag = false;
+    for (size_t i = 1; i < args_.size(); i++) {
+        const auto &current_arg = args_.at(i);
+        size_t equal_pos = current_arg.find('=');
+        // イコールが存在する場合は、名前付き引数として扱う。
+        if (equal_pos != std::string::npos) {
+            named_arg_flag = true;
+        }
+        // 名前付き引数をパースする。
+        if (named_arg_flag) {
+            std::string key, value;
+            // 一回目に出現したイコールを基準にkeyとvalueにパースする。
+            // valueにのみイコールを含むことが可能です。
+            key = current_arg.substr(0, equal_pos);
+            value = current_arg.substr(equal_pos + 1);
+            // keyが空なら、登録しない。
+            if (key.empty()) {
+                break;
+            }
+            // valueが空なら、登録しない。
+            if (value.empty()) {
+                break;
+            }
+            // mapに格納する。
+            // 名前付き引数(任意)
+            _named_arg.try_emplace(key, value);
+        } else {
+            // フラグ引数を格納する。
+            if (std::regex_match(current_arg, std::regex("(^-[^-]$)|(^--[^-].+$)"))) {
+                _named_arg.try_emplace(current_arg, "true");
+            } else {
+                // イコールを含まない引数を格納する。
+                // 引数(必須)
+                _args.emplace_back(current_arg);
+            }
+        }
+    }
+}
+
+com_sakushira::cpp_lib::ArgumentParser::ArgumentParser() : _args(), _named_arg() {}
+
+const std::vector<std::string> &com_sakushira::cpp_lib::ArgumentParser::get_args() const {
+    return _args;
+}
+
+std::string com_sakushira::cpp_lib::ArgumentParser::get_arg(size_t i) const {
+    if (i < _args.size()) {
+        return _args.at(i);
+    }
+    return "";
+}
+
+std::string com_sakushira::cpp_lib::ArgumentParser::get_named_arg(const std::string &key) const {
+    if (_named_arg.contains(key)) {
+        return _named_arg.at(key);
+    }
+    return "";
+}
+
+bool com_sakushira::cpp_lib::ArgumentParser::is_exist_named_arg(const std::string &key) const {
+    return _named_arg.contains(key);
+}
+
+int64_t com_sakushira::cpp_lib::ArgumentParser::get_named_arg_int64(const std::string &key) const {
+    auto val = this->get_named_arg(key);
+    if (!std::regex_match(val, std::regex(R"(-?(0|[1-9][0-9]*))"))) {
+        throw std::runtime_error("Argument value is not number.");
+    }
+    return std::stoll(val);
+}
+
+int64_t
+com_sakushira::cpp_lib::ArgumentParser::get_named_arg_int64(const std::string &key,
+                                                            int64_t default_value) const noexcept {
+    try {
+        return get_named_arg_int64(key);
+    } catch (std::exception &e) {
+        return default_value;
+    }
+}
+
+void com_sakushira::cpp_lib::ArgumentParser::parse(int argc, char **argv) {
+    std::vector<std::string> args(argv, argv + argc);
+    this->parse(args);
+}
