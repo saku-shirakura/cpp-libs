fe1879100f858dff266605661168cd850e3edb42
saku shirakura <saku@sakushira.com>
2024-12-08 23:37:49 +0900

    [Add] 長い型の短縮用エイリアスを定義

---
+++ include/com_sakushira_cpp_lib/shorthand.h
@@ -0,0 +1,41 @@
+// MIT License
+//
+// Copyright (c) 2024 Saku Shirakura <saku@sakushira.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/**
+ * @file com_sakushira_cpp_lib/shorthand.h
+ * @date 24/12/06
+ * @brief long longのような型の省略形を定義しています。
+ * @author saku shirakura (saku@sakushira.com)
+ */
+
+
+#ifndef CPP_LIBS_SHORTHAND_H
+#define CPP_LIBS_SHORTHAND_H
+
+using sll = long long; // signed long longよりsll
+using us = unsigned short;
+using ui = unsigned int;
+using ul = unsigned long;
+using ull = unsigned long long;
+using uchar = unsigned char;
+
+#endif //CPP_LIBS_SHORTHAND_H
